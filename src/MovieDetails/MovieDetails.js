69e1f053b984087856f25c5ea5be1297f792d4c5
Logan Sauer <ldsauer2@gmail.com>
2025-04-08 18:11:49 -0600

    commiting to pull down the refactors that Rig has done

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -1,12 +1,22 @@
 import './MovieDetails.css';
-import movieDetails from '../data/movie_details';
+// import movieDetails from '../data/movie_details';
 import { useState, useEffect } from 'react';
 
 
 function MovieDetails({ movie }) {
   const [movieDetails, setMovieDetails] = useState(null);
 
-  
+  useEffect(() => {
+    fetch(`https://rancid-tomatillos-api-ce4a3879078e.herokuapp.com//api/v1/movies/${movie.id}`)
+    .then(response => response.json())
+    .then(data => {console.log("API response:", data); setMovieDetails(data.movie)})
+    .catch(error => console.error("There was an error fetching movie details:", error));
+  }, [movie.id]);
+
+  if (!movieDetails) {
+    return <p>No details found for this movie</p>
+  }
+
   return (
     <section className='MovieDetails'>
       <img src={movie.backdrop_path} alt={`${movieDetails.title} poster`}/>
