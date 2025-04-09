b22dcf5a2f2f114f498ba8a6170e494ffcc80823
Logan Sauer <ldsauer2@gmail.com>
2025-04-08 19:29:51 -0600

    finished up my refactor. Pushing up for a PR

--- src/App/App.js
+++ src/App/App.js
@@ -11,56 +11,55 @@ import { fetchMovies, updateMovieVotes } from '../utilities/api';
 import MovieDetails from '../MovieDetails/MovieDetails';
 
 function App() {
-
   const [chosenMovie, setChosenMovie] = useState(null)
 
   function handlePosterClick(movie) {
     setChosenMovie(movie)
   }
-
+  
   function goHome() {
     setChosenMovie(null);
   }
-
+  
   const [movies, setMovies] = useState([]);
-
+  
   useEffect(() => {
     fetchMovies()
-      .then((movieData) => {setMovies(movieData)})
-      .catch((error) => {console.error('Something went wrong while fetching movie data:', error)})
+    .then((movieData) => {setMovies(movieData)})
+    .catch((error) => {console.error('Something went wrong while fetching movie data:', error)})
   }, [])
-
+  
   const addVote = (movieId) => {
     updateMovieVotes(movieId, 'up')
-      .then(setMovies(movies.map((movie) => 
-        (movie.id === movieId) ? { ...movie, vote_count: movie.vote_count + 1 } : movie
-      )
-    ))
-  }
+    .then(setMovies(movies.map((movie) => 
+      (movie.id === movieId) ? { ...movie, vote_count: movie.vote_count + 1 } : movie
+  )
+))
+}
 
-  const subtractVote = (movieId) => {
-    updateMovieVotes(movieId, 'down')
-      .then(setMovies(movies.map((movie) => 
-        (movie.id === movieId) ? { ...movie, vote_count: movie.vote_count - 1 } : movie
-      )
-    ))
-  }
+const subtractVote = (movieId) => {
+  updateMovieVotes(movieId, 'down')
+  .then(setMovies(movies.map((movie) => 
+    (movie.id === movieId) ? { ...movie, vote_count: movie.vote_count - 1 } : movie
+)
+))
+}
 
-  return (
-    <main className='App'>
+return (
+  <main className='App'>
       <header>
         <h1>rancid tomatillos</h1>
         <div></div>
-        {chosenMovie && (<button class="homeButton" onClick={goHome}><img src={homeIcon} alt="Home Button" /></button>)}
+        {chosenMovie && (<button className="homeButton" onClick={goHome}><img src={homeIcon} alt="Home Button" /></button>)}
       </header>
       {chosenMovie ? ( 
-        <MovieDetails movie={movieDetails} goHome={goHome} /> 
-        ) : ( 
-          <MoviesContainer 
-          movies={movies} 
-          onAddVote={addVote} 
-          onSubtractVote={subtractVote}
-          onPosterClick={handlePosterClick} 
+        <MovieDetails movie={chosenMovie} goHome={goHome} /> 
+      ) : ( 
+        <MoviesContainer 
+        movies={movies} 
+        onAddVote={addVote} 
+        onSubtractVote={subtractVote}
+        onPosterClick={handlePosterClick} 
         />
       )}
     </main>
--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -2,14 +2,13 @@ import './MovieDetails.css';
 // import movieDetails from '../data/movie_details';
 import { useState, useEffect } from 'react';
 
-
 function MovieDetails({ movie }) {
   const [movieDetails, setMovieDetails] = useState(null);
 
   useEffect(() => {
-    fetch(`https://rancid-tomatillos-api-ce4a3879078e.herokuapp.com//api/v1/movies/${movie.id}`)
+    fetch(`https://rancid-tomatillos-api-ce4a3879078e.herokuapp.com/api/v1/movies/${movie.id}`)
     .then(response => response.json())
-    .then(data => {console.log("API response:", data); setMovieDetails(data.movie)})
+    .then(data => setMovieDetails(data))
     .catch(error => console.error("There was an error fetching movie details:", error));
   }, [movie.id]);
 
@@ -19,14 +18,14 @@ function MovieDetails({ movie }) {
 
   return (
     <section className='MovieDetails'>
-      <img src={movie.backdrop_path} alt={`${movieDetails.title} poster`}/>
-      <h2 className='movie-title'>{movie.title}</h2>
+      <img src={movieDetails.backdrop_path} alt={`${movieDetails.title} poster`}/>
+      <h2 className='movie-title'>{movieDetails.title}</h2>
       <div className="genre-container">
-        {movie.genre_ids.map((genre, index) => (
+        {movieDetails.genre_ids.map((genre, index) => (
           <div key={index} className="genre-tag">{genre}</div>
         ))}
       </div>
-      <p className="movie-overview">{movie.overview}</p>
+      <p className="movie-overview">{movieDetails.overview}</p>
     </section>
   );
 }
