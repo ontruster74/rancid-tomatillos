37b417d47a1e6958521f848bc605c4b145464a96
ontruster74 <ontruster74@gmail.com>
2025-04-12 14:23:14 -0700

    fix NotFound styling

--- src/NotFound/NotFound.js
+++ src/NotFound/NotFound.js
@@ -2,6 +2,7 @@ function NotFound() {
     return (
         <div className="error-message" data-testid="not-found-page">
             <h2>404 - Not Found</h2>
+            <div></div>
             <p className="error-message">This page does not exist in our website.</p>
         </div>
     )
